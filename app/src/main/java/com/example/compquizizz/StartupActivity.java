1823036d9d5e08d77b264e3d7371c3ff6d57c21f
ALIF SAFWAN <alifsafwanz30@gmail.com>
2024-01-10 23:54:37 +0800

    Add java class for history, login, startup

---
+++ app/src/main/java/com/example/compquizizz/StartupActivity.java
@@ -0,0 +1,13 @@
+package com.example.compquizizz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}
