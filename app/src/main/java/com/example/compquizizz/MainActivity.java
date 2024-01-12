e4a7511c751d5b7a8099e74b2d1a43eddd1ee385
Hazrul <hazrulidham28@gmail.com>
2024-01-12 22:28:12 +0800

    add login checker in Main activity

--- app/src/main/java/com/example/compquizizz/MainActivity.java
+++ app/src/main/java/com/example/compquizizz/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.compquizizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+            // check if the user is already signed in.. direct to the main menu
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null){
+                // is mean that the user isn't logged out from last session
+
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Intent intent = new Intent(MainActivity.this,StartupActivity.class);
+                startActivity(intent);
+            }
     }
 }
